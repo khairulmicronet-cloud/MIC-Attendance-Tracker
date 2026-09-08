f457fc6932521c67514db656729ae7f2e4512f53
Khairul Github <khairul.micronet@gmail.com>
2026-09-08 19:54:24 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "mic-attendance-v1";
+const APP_SHELL = [
+      "./index.html",
+      "./manifest.json",
+      "./logo.png",
+      "./icons/icon-192.png",
+      "./icons/icon-512.png"
+  ];
+
+self.addEventListener("install", (event) => {
+      event.waitUntil(
+                caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+            );
+      self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+      event.waitUntil(
+                caches.keys().then((keys) =>
+                              Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+                                           )
+            );
+      self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+      if (event.request.url.includes("nominatim.openstreetmap.org")) {
+                return;
+      }
+      event.respondWith(
+                caches.match(event.request).then((cached) => cached || fetch(event.request))
+            );
+});
